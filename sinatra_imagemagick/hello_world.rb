b0417210c0263c49a090cd3e1b1536b58d168ccf
Zach Morek <zmorek@gmail.com>
2013-04-07 19:51:24 -0400

    test RMagick

--- sinatra_imagemagick/hello_world.rb
+++ sinatra_imagemagick/hello_world.rb
@@ -1,9 +1,23 @@
-require 'sinatra'
+# require 'sinatra'
+#
+# get '/' do
+#   erb :index
+# end
+#
+# get '/say_hello' do
+#   erb :say_hello
+# end
 
-get '/' do
-  erb :index
-end
+require 'RMagick'
 
-get '/say_hello' do
-  erb :say_hello
-end
+# http://studio.imagemagick.org/RMagick/doc/
+image = Magick::ImageList.new("Ruby_logo.png")
+text = Magick::Draw.new
+text.annotate(image, 0, 0, 0, 60, "<3 Ruby") {
+    self.gravity = Magick::SouthGravity
+    self.pointsize = 72
+    self.stroke = '#000000'
+    self.fill = '#FFFFFF'
+    self.font_weight = Magick::BoldWeight
+    }
+image.write('Less_than_three.png')
