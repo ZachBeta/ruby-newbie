f625543c88c36cbcd206a0fd4e8c860cf85344f4
Zach Morek <zmorek@continuity.net>
2015-05-26 17:43:26 -0400

    fix image gallery

--- sinatra_gallery/hello_world.rb
+++ sinatra_gallery/hello_world.rb
@@ -4,7 +4,7 @@ require 'httparty'
 
 get '/' do
   response = HTTParty.get("http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC")
-  @image_urls = JSON.parse(response)['data'].first['images'].first.last['url'].to_a
+  @image_urls = [JSON.parse(response.body)['data'].first['images'].first.last['url']]
 
   erb :index
 end
